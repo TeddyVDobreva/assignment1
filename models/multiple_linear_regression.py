f31381dff3ab675bfcbc01af129e827959117376
TeddyVDobreva <t.v.dobreva@student.rug.nl>
2025-09-10 16:42:15 +0200

    Validate function and fixed dot matrix multiplication

--- models/multiple_linear_regression.py
+++ models/multiple_linear_regression.py
@@ -11,16 +11,6 @@ class MultipleLinearRegressor:
         self._parameters = dict(parameters= default_parameters)
 
     def fit(self, observations: np.ndarray, ground_truth: np.ndarray) ->  None:
-        """
-        DO  WE CHECK
-
-        The observations and ground truth should be np.ndarrays. Make
-        sure the number of samples is in the row dimension, while the
-        variables are in the column dimension.
-        """
-
-
-
 
         """
         A function that calculates the parameters as from Equation based on a given training dataset 
@@ -35,29 +25,35 @@ class MultipleLinearRegressor:
         Returns:
         None
         """
-        num_samples = observations.shape[0]
-        #add colums of 1s to the observations as observations_prime
-        addition = np.ones((num_samples, 1))
-        observations_prime = np.column_stack((observations, addition))
-        #find the transpose observations_prime
-        trans_observations = observations.transpose()
-        #implement formula
-        optimal_parameters = ((trans_observations * observations_prime) ** (-1)) * trans_observations * ground_truth
-        self._parameters['parameters'] = optimal_parameters
-        #turn it into a dict
+        #calculate if observations and ground truth have equal nums of rows
+        if self.__validate_observations_ground_truth(observations, ground_truth):
+            num_samples = observations.shape[0] 
+            #add colums of 1s to the observations as observations_prime
+            addition = np.ones((num_samples, 1))
+            observations_prime = np.column_stack((observations, addition))
+            #find the transpose observations_prime
+            trans_observations = observations_prime.transpose()
+            #implement formula
+            optimal_parameters = np.dot(np.dot((np.dot(trans_observations, observations_prime) ** (-1)), trans_observations), ground_truth)
+            self._parameters['parameters'] = optimal_parameters
 
     def predict(self, observations: np.ndarray) -> np.ndarray:
         """
         Return the prediction from observations based on the parameters
         """
-        return [[key * self._parameters[col] for col, key in enumerate[row]] for row in observations]
+        #add colums of 1s to the observations as observations_prime
+        addition = np.ones((observations.shape[0], 1))
+        observations_prime = np.column_stack((observations, addition))
+        return np.dot(observations_prime, self._parameters['parameters'])
     
-    def get_parameters(self) -> np.ndarray:
+    @property
+    def parameters(self) -> np.ndarray:
+        """
+        Getter: Provides a read only view of the parameters
+        """
         return self._parameters['parameters']
-
-
-
-
-
-
-
+    
+    def __validate_observations_ground_truth(self, observations: np.ndarray, ground_truth: np.ndarray) -> bool:
+        #check if observations and ground thruth have the same amount of rows (data points)
+        if observations.shape[0] != ground_truth.shape[0]:
+            return False
