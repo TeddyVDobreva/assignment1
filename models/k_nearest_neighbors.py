9a14d0575e4fe3b7be5e9a596b3ac0c74d2ac8f9
TeddyVDobreva <t.v.dobreva@student.rug.nl>
2025-09-11 14:39:38 +0200

    Paste the code from Tutorial 2

--- models/k_nearest_neighbors.py
+++ models/k_nearest_neighbors.py
@@ -1,50 +1,59 @@
 import numpy as np
-import math
+from collections import Counter
 
 
 
 class KNNeighbours:
     def __init__(
-        self, k: int = 3, obseravtions: np.ndarray = 0, ground_truth: np.ndarray = 0
-    ) -> None:
+        self, k: int = 3
+        ) -> None:
         """
         Constructor
         """
-        if self.validate_k(k):
+        if self.__validate_k(k):
             self._k = k
-        self._observations = obseravtions
-        self._ground_truth = ground_truth
+        self._parameters = {}
 
-    def predict(self, new_observation: np.ndarray) -> str:
+    def fit(self, observations: np.ndarray, ground_truth: np.ndarray) -> None:
+        """
+        Fit function for the k-nearest neighbours algorithm
+        """
+        #check dimentions of observations and ground truth
+        if self.__validate_observations_ground_truth(observations, ground_truth):
+            self._parameters["observations"] = observations
+            self._parameters["ground_truth"] = ground_truth
+
+    def __predict_single(self, x: np.ndarray) -> int:
+        """
+        Private function to determine the ground truth for a singular new observation
+        """
+        distances = np.linalg.norm(self._parameters["observations"] - x, axis=1)
+        nn_indices = np.argsort(distances)[:self._k]
+        nn_ground_truths = self._parameters["ground_truth"][nn_indices]
+        most_common = Counter(nn_ground_truths).most_common(1)
+        return most_common[0][0]
+
+
+    def predict(self, new_observation: np.ndarray) -> np.ndarray:
         """
         A function that predicts the ground truth for some new observations
         based on the k-nearest neighbours algorithm
         """
-        # init the dictionary to store the nearest k neighbours
-        k_dict = {x: float("inf") for x in range(self._k)}
-        # go through all points to find the closest neighbour
-        for row in self._observations:
-            # find the distance
-            distance = 0
-            for i in range(row.size()):
-                distance += (row[i] - new_observation[i]) ** 2
-            distance = math.sqrt(distance)
+        pred = [self.__predict_single(x) for x in new_observation]
+        return np.array(pred)
 
-            # find the var with the largest distance in the k_dict
-            largest_dist_var = max(k_dict, key=k_dict.get)
-            # check if its bigger thant the new distance
-            if distance < k_dict[largest_dist_var]:
-                # if it is not, replace it
-                del k_dict[largest_dist_var]
-                k_dict[row] = distance
-
-        # find the number each ground thruth occurs in the knns
-        ground_truth_occcurance = {x: 0 for x in self._ground_truth}
-        for key in k_dict:
-            key_ground_truth = self._ground_truth[key]
-            ground_truth_occcurance[key_ground_truth] += 1
-        # return the most common ground thruth
-        return max(ground_truth_occcurance, key=ground_truth_occcurance.get)
     
-    def validate_k(self, k:int) -> bool:
+    def __validate_k(self, k:int) -> bool:
+        """
+        Validator for the k value, since it should be greater than 0
+        """
         return k > 0
+
+    def __validate_observations_ground_truth(
+        self, observations: np.ndarray, ground_truth: np.ndarray
+    ) -> bool:
+        """
+        Validator for the observations and ground truth, since they have to have the same number of rows (data points)
+        """
+        return observations.shape[0] == ground_truth.shape[0]
+
