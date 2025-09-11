beb10d99d2a59182c6a78ce3fe8277aaca59ab3e
TeddyVDobreva <t.v.dobreva@student.rug.nl>
2025-09-11 15:11:22 +0200

    Update knn in main according to Tutorial 2 code

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     mlr_observations = wine_dataset[:1200, :-1]
 
     # Separating some data for predictions
-    test_observations = wine_dataset[1200:, :-1]
+    mlr_test_observations = wine_dataset[1200:, :-1]
 
     # Instantation of the Multiple Linear Regressor
     regression_model = multiple_linear_regression.MultipleLinearRegressor()
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     # Training the model
     regression_model.fit(mlr_observations, mlr_ground_truth)
     # Prediction based on found parameters
-    mlr_prediction = regression_model.predict(test_observations)
+    mlr_prediction = regression_model.predict(mlr_test_observations)
 
     # Printing of the parameters
     print(mlr_prediction)
@@ -30,14 +30,21 @@ if __name__ == "__main__":
     iris_df = pd.read_csv("data/iris.csv", delimiter=",")
     iris_dataset = iris_df.values
 
-    # Getting the observations
-    knn_observations = iris_dataset[:, :-1]
+    # Getting the observations and ground truth
+    knn_observations = iris_dataset[:113, :-1]
+    knn_ground_truth = iris_dataset[:113, -1]
+
+    # Separating some data for predictions
+    knn_test_observations = iris_dataset[113:, :-1]
 
     # Instantation of the K-Nearest Neighbors model
-    neighbors = k_nearest_neighbors.KNNeighbours()
+    knn = k_nearest_neighbors.KNNeighbours()
+
+    # Train the model
+    knn.fit(knn_observations, knn_ground_truth)
 
     # Prediction of new observations
-    knn_prediction = neighbors.predict(knn_observations)
+    knn_prediction = knn.predict(knn_test_observations)
 
     # Printing the ground truth
     print(knn_prediction)
